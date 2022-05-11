a1a035a1f01b1432dcb42281e8d89d72a8dab68e
Ofek Ben Baruch <94955256+Ofekbb@users.noreply.github.com>
2022-05-11 12:08:13 +0300

    configure github actions

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
-app.get('/names',(req,res)=>{
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('/names', (req, res) => {
     res.send("Discount Bank")
 })
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log("Server is running on port 5000")
 })

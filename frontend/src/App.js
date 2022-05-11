54b4c0b972ef2700d27ffe14977dfdf962923ffd
Ofek Ben Baruch <94955256+Ofekbb@users.noreply.github.com>
2022-05-11 11:13:43 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [userName,setUsername] = useState('')
+
+  useEffect(()=>{
+    getNames()
+
+  })
+
+  const getNames = async() => {
+    const response = await axios.get('/names')
+    setUsername(response.data)
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello Simple React</h1>
+      <h3>Welcome {userName}</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
